cf1dd37a7683f6d9f5da354b446a49fdee6408fd
davitgurabanidze <59085432+davitgurabanidze@users.noreply.github.com>
2020-01-26 16:21:41 +0400

    last 26
    last26

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+let i = 0;
+while (i <= 3) {
+	console.log(i)
+	i++;
+}
+console.log("end of while")
+console.log("start of do while")
+let a = 1;
+do {
+	console.log(a)
+	a++;
+} while (a < 3)
+
+console.log("for loop");
+
+for (let b = 0; b < 4; b++) {
+	console.log(b)
+}
