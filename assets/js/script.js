5180ddb05eca62b6dca7f36d1b497bedcfe2b0d4
davitgurabanidze <59085432+davitgurabanidze@users.noreply.github.com>
2020-01-27 21:06:03 +0400

    change
     change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,11 @@
-let i = 0;
-while (i <= 3) {
-	console.log(i)
-	i++;
-}
-console.log("end of while")
-console.log("start of do while")
-let a = 1;
-do {
-	console.log(a)
-	a++;
-} while (a < 3)
-
-console.log("for loop");
-
-for (let b = 0; b < 4; b++) {
-	console.log(b)
+let add = document.querySelectorAll('.menu_ul a');
+// console.log(add);
+let bl = add.length;
+// console.log(bl)
+for (let i = 0; i < bl; i++) {
+	
+	add[i].addEventListener('click', function(e) {
+		console.log(i)
+		this.classList.toggle('added');
+});
 }
