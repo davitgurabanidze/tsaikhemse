bc59f4c8edfeac1ef6e1d4db8c2cf520a8c82772
davitgurabanidze <59085432+davitgurabanidze@users.noreply.github.com>
2020-01-27 21:30:01 +0400

    add
    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,11 @@ for (let i = 0; i < bl; i++) {
 	add[i].addEventListener('click', function(e) {
 		console.log(i)
 		this.classList.toggle('added');
+		if (this.classList.contains('added')) {
+			this.textContent = "დამატებულია";
+
+        } else {
+        	this.textContent = "დამატება";
+        }
 });
 }
